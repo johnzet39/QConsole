65c96dbf473d663aee4066acf1f0f03046685b39
Evgeniy Z <johnzet@yandex.ru>
2019-06-06 14:49:41 +0200

    Update AssemblyInfo1.cs

--- QConsole/Properties/AssemblyInfo1.cs
+++ QConsole/Properties/AssemblyInfo1.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.3435.228")]
-[assembly: AssemblyFileVersion("1.3.3435.228")]
+[assembly: AssemblyVersion("1.3.3443.375")]
+[assembly: AssemblyFileVersion("1.3.3443.375")]
