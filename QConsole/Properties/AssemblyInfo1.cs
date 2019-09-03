ed9117c4c6a53699232bcd360e37952d0ccd845a
Evgeniy Z <johnzet@yandex.ru>
2019-09-03 17:52:07 +0200

    IsReadOnly for username textbox.

--- QConsole/Properties/AssemblyInfo1.cs
+++ QConsole/Properties/AssemblyInfo1.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.3490.266")]
-[assembly: AssemblyFileVersion("1.3.3490.266")]
+[assembly: AssemblyVersion("1.3.3532.475")]
+[assembly: AssemblyFileVersion("1.3.3532.475")]
